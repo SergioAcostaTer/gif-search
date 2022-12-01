f996926300fa714a12143cde2336010907b92974
Sergio Acosta <sergioacostaquintana@gmail.com>
2022-12-01 01:30:34 +0000

    huge improvements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
+import Search from "./pages/Search";
+
 import "./normalize.css"
+import "./imports.css"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path={"/"} element={<Main />}/>
+        <Route path={"/search"} element={<Search />}/>
       </Routes>
     </>
   );
